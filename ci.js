e34f074d2f25b7382e6477a3335465629d7b0f72
William Estoque <william.estoque@gmail.com>
2013-07-31 23:32:03 -0700

    Initial working resize

--- ci.js
+++ ci.js
@@ -48,7 +48,7 @@ var ci = {
     var $resizers = $('#header table th');
     for (var i = 1; i < $resizers.length; i += 1) {
       var view = new ci.Views.Resizer({$column: $resizers.eq(i)});
-      view.$el.attr('data-index', i);
+      view.$el.attr('data-index', i - 1);
       this.resizers[i] = view;
       $(document.body).append(view.render().el);
     }
@@ -75,11 +75,24 @@ var ci = {
   },
 
   _onResizerDrag: function(e) {
-    var index = $(e.target).attr('data-index');
-    console.log('drag');
-    //this.trigger('drag', e);
-  }
+    if (e.x === 0 && e.y === 0) { return; }
 
+    var index = parseInt($(e.target).attr('data-index'));
+    var thWidth = $('#header table th').eq(index)[0].offsetLeft + $('#header table th').eq(index)[0].offsetWidth;
+    var difference = thWidth - e.x;
+    var percentage = (difference / $('#header table').width()) * 100;
+
+    // Resize the column
+    var newColWidth = $('#header table col').eq(index).width() - percentage;
+    $('#header table col').eq(index).css('width', newColWidth + '%');
+    $('#content table col').eq(index).css('width', newColWidth + '%');
+
+    var newColWidth = $('#header table col').eq(index + 1).width() + percentage;
+    $('#header table col').eq(index + 1).css('width', newColWidth + '%');
+    $('#content table col').eq(index + 1).css('width', newColWidth + '%');
+
+    this.trigger('resize');
+  }
 };
 
 // Make sure we get Backbone events on our
