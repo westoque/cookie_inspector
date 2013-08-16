c9c8b0a9a7c1543819a198f4cbf03454fe644dff
William Estoque <william.estoque@gmail.com>
2013-08-15 18:37:07 -0700

    Cleaned up footer

--- views/footer.js
+++ views/footer.js
@@ -2,10 +2,6 @@ ci.Views.Footer = Backbone.View.extend({
 
   id: 'footer',
 
-  events: {
-    'click #new-cookie' : '_onAddNewCookieClick'
-  },
-
   template: function() {
     return Hogan.compile($('#footer-tmpl').text());
   },
@@ -13,29 +9,6 @@ ci.Views.Footer = Backbone.View.extend({
   render: function() {
     this.$el.html(this.template().render());
     return this;
-  },
-
-  _onAddNewCookieClick: function(e) {
-    e.preventDefault();
-
-    // TODO: Move this somewhere.
-    chrome.devtools.inspectedWindow.eval('window.document.domain', function(domain) {
-      var cookie = new ci.Models.Cookie({
-        domain: domain,
-        expirationDate: (new Date().getTime() / 1000),
-        hostOnly: false,
-        httpOnly: false,
-        name: 'Cookie',
-        path: '/',
-        secure: false,
-        session: false,
-        value: 'Value'
-      });
-
-      var view = new ci.Views.CookieForm({ model: cookie });
-      $(document.body).append(view.render().el);
-      view.$('input').eq(0).focus();
-    });
   }
 
 });
