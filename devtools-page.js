5adb3d803b2425f1dca41c6943bd5f7bc1d8f921
William Estoque <william.estoque@gmail.com>
2013-08-18 00:05:19 -0700

    Added images

--- devtools-page.js
+++ devtools-page.js
@@ -3,7 +3,7 @@
   fetchDevToolsAPI(function(is_remote) {
     if (chromeDevTools && chromeDevTools.panels) {
       chromeDevTools.panels.create('Cookies',
-                                   'pagespeed-devtools-icon.png',
+                                   'cookie-icon.png',
                                    'CookieInspectorChromium.html');
     } else {
       alert('Chrome DevTools extension API is not available.');
